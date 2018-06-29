b116c427e9a7c36db3bbf91cedf9da0312dc27d7
Morgan Kane <morgan@thrivera.io>
2018-06-29 16:36:36 -0400

    getting components ready to set up routes

---
+++ src/components/contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">Contact us</h4>
+      <p class="card-text">
+        <address>
+          <strong>Pizza Planet</strong><br>
+          1100 Sycamore St<br>
+          Floor 7<br>
+          Cincinnati, OH<br>
+          Phone: 581-284-7371<br>
+        </address>
+        <address>
+          <strong>Email:</strong>
+          <p>contact@pizzaplanet.com</p>
+        </address>
+      </p>
+    </div>
+  </div>
+</template>
