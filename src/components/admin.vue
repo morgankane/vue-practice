7fc371636732acbd6252ef36c8ca9dffad066992
Morgan Kane <morgan@thrivera.io>
2018-06-29 10:57:32 -0400

    admin second table - all static content

--- src/components/admin.vue
+++ src/components/admin.vue
@@ -1,24 +1,53 @@
 <template>
-  <div class="row">
-    <div class="col-md-6 col-sm-12">
-      <!--new pizza component-->
+  <div>
+    <div class="row">
+      <div class="col-md-6 col-sm-12">
+        <!--new pizza component-->
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <h3>Menu:</h3>
+        <table class="table table-hover">
+          <thead class="thead-default">
+            <tr>
+              <th>Item</th>
+              <th>Remove from Menu</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Margherita</td>
+              <td><button class="btn btn-outline-danger btn-sm">x</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div class="col-md-6 col-sm-12">
-      <h3>Menu:</h3>
-      <table class="table table-hover">
-        <thead class="thead-default">
-          <tr>
-            <th>Item</th>
-            <th>Remove from Menu</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Margherita</td>
-            <td><button class="btn btn-outline-danger btn-sm">x</button></td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="row">
+      <div class="col-sm-12">
+        <h3>Current Orders</h3>
+        <table class="table table-sm">
+          <thead class="thead-default">
+            <tr>
+              <th>Item</th>
+              <th>Size</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <div class="order-number">
+              <strong><em>Order Number: 1</em></strong>
+              <button class="btn btn-outline-danger btn-sm">x</button>
+            </div>
+            <tr>
+              <td>Margherita</td>
+              <td>9"</td>
+              <td>1</td>
+              <td>9.95</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
