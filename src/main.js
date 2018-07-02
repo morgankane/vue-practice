02aea6d2dee219802b007cd56c000cefe5047bc2
Morgan Kane <morgan@thrivera.io>
2018-07-02 16:59:49 -0400

    global navigation guards - stopping someone from accessing certain links

--- src/main.js
+++ src/main.js
@@ -30,6 +30,14 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.path === "/mainmenu") {
+    next()
+  } else {
+    next(false);
+  }
+})
+
 new Vue({
   el: '#app',
   router,
