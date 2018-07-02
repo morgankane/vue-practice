db0d092b384b7a5568f6b53b67433cebd2465e7c
Morgan Kane <morgan@thrivera.io>
2018-07-02 16:40:25 -0400

    navigation for button - @onclick taking you to specific page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,12 +5,22 @@
         <div class="background">
             <h1>Welcome to Pizza Planet!</h1>
             <h2>Feeling Hungry?</h2>
-            <button class="btn btn-success">Let's order!</button>
+            <button class="btn btn-success" @click="goToMenu">Let's order!</button>
         </div>
       </div>
     </div>
 
 </template>
+
+<script>
+export default {
+  methods: {
+    goToMenu() {
+      this.$router.push('/mainmenu')
+    }
+  }
+}
+</script>
 
 <style>
 #home {
