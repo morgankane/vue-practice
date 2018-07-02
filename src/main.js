4dd462c5679d9be5633f72843eb12c25cd4ff870
Morgan Kane <morgan@thrivera.io>
2018-07-02 17:38:09 -0400

    refactor routes into own file to keep main.js clean

--- src/main.js
+++ src/main.js
@@ -1,32 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from './components/Home'
-import mainmenu from './components/mainmenu'
-import about from './components/about'
-import contact from './components/contact'
-import history from './components/history'
-import delivery from './components/delivery'
-import orderingGuide from './components/orderingGuide'
-import admin from './components/admin'
 import App from './App.vue'
+import { routes } from './routes'
 
 Vue.use(VueRouter)
-
-const routes = [
-  { path: '/', component: Home },
-  { path: '/mainmenu', component: mainmenu },
-  { path: '/admin', component: admin, beforeEnter: (to, from, next) => {
-    alert('This area is for authorized users only, please login to continue.');
-    next()
-  }},
-  { path: '/about', component: about, children: [
-      { path: '/contact', component: contact},
-      { path: '/history', component: history},
-      { path: '/delivery', component: delivery},
-      { path: '/orderingGuide', component: orderingGuide }
-    ]},
-  { path: '*', redirect: '/' }
-]
 
 const router = new VueRouter({
   mode: 'history',
---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import Home from './components/Home'
+import mainmenu from './components/mainmenu'
+import about from './components/about'
+import contact from './components/contact'
+import history from './components/history'
+import delivery from './components/delivery'
+import orderingGuide from './components/orderingGuide'
+import admin from './components/admin'
+
+export const routes = [
+  { path: '/', component: Home },
+  { path: '/mainmenu', component: mainmenu },
+  { path: '/admin', component: admin, beforeEnter: (to, from, next) => {
+    alert('This area is for authorized users only, please login to continue.');
+    next()
+  }},
+  { path: '/about', component: about, children: [
+      { path: '/contact', component: contact},
+      { path: '/history', component: history},
+      { path: '/delivery', component: delivery},
+      { path: '/orderingGuide', component: orderingGuide }
+    ]},
+  { path: '*', redirect: '/' }
+]
